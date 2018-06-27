27e24043d11105a139b0e88d49c8f8442a53cd55
harrison.henderson.stevens <harrison.henderson.stevens@gmail.com>
2018-06-27 15:22:55 -0400

    this has the simple task of testing the users entry, if they enter to many or not enough parameters an error is thrown

---
+++ parameters.js
@@ -0,0 +1,18 @@
+//This will take the user inputted parameter, it will then use the try-catch-finally statement, the idea being if the user enters too many parameters 
+//or too little then an error will be thrown 
+'use strict'
+const readline = require('readline-sync')
+
+const Paramcount = 3
+console.log(process.argv)
+try {
+if (process.argv.length != Paramcount) {
+  throw new Error('missing parameters')
+}
+}catch(Error) {
+  console.log("Incorrect")
+  return
+}finally {
+  console.log("try Again")
+  
+}
