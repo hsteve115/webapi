d14206887decca9320a301ee0c6f40bb7bc21655
harrison.henderson.stevens <harrison.henderson.stevens@gmail.com>
2018-06-27 16:17:53 -0400

    Looking at the use of Arrow functions, this arrow function works on dividing a number by another, getting the closet full value. this is the returned as a value from where the function was called

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -3,3 +3,15 @@
 'use strict'
 
 const readline = require("readline-sync")
+//This will create a local callable function, which will return a value to the command line
+const remainder2 = (dividend, divisor) => {
+  //largest integer less than or equal to a given number.
+	const quotient = Math.floor(dividend / divisor)
+  // so this takes the first value divides it by the second, from here a quotient is worked out with is a integrer value not floating point
+  //This is then taken away from the original value which was being divided by. then returns this value to where the function was originally 
+  //called from
+	return dividend - quotient
+}
+const whatsleft = remainder2(2,5)
+console.log(remainder2(22,9))
+console.log(whatsleft)
