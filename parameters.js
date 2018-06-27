3883d9734d5cdc546e694886f38a4e7f7a0799b4
harrison.henderson.stevens <harrison.henderson.stevens@gmail.com>
2018-06-27 15:26:36 -0400

    this has the simple task of testing the users entry,and paramters, this updates previous by giving different erros

--- parameters.js
+++ parameters.js
@@ -6,13 +6,15 @@ const readline = require('readline-sync')
 const Paramcount = 3
 console.log(process.argv)
 try {
-if (process.argv.length != Paramcount) {
-  throw new Error('missing parameters')
-}
+if (process.argv.length > Paramcount) {
+  throw new Error('You have entered too many parameters')
+}else if (process.argv.length < Paramcount) {
+   throw new Error('missing parameters')
+          }
 }catch(Error) {
-  console.log("Incorrect")
+  console.log("Incorrect: " + Error)
   return
 }finally {
-  console.log("try Again")
+  console.log("Please reload")
   
 }
